f96b1f04df3e1c2f28fe2f0829f6eb0384bb2173
Mujib <mujib517@gmail.com>
2021-10-05 10:48:04 +0530

    Rect app component, mounting

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
 import React from 'react';
-import './App.css';
-import MathObj, { PI } from './Math';
-
-class Counter extends React.Component {
-  state = { count: 0 };
-
-  render() {
-    return <div>
-      <h1>Count: {this.state.count}</h1>
-      <button>++</button>
-    </div>
-  }
-}
-
-// function Counter() {
-//   const count = 0;
-
-//   return <div>
-//     <h1>Count: {count}</h1>
-//     <button>++</button>
-//   </div>
-// }
 
 function App() {
-  var m1 = new MathObj();
-  const res = m1.add(10, 20);
-  return (
-    <div className="App">
-      <h1>Hello React!! {res} {PI}</h1>
-      <Counter></Counter>
-    </div>
-  );
+    return <h1>Hello React</h1>
 }
 
 export default App;
