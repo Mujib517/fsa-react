8e40133d2d166b16b4ca1cd3108ab20dbef939a1
Mujib <mujib517@gmail.com>
2021-10-08 10:36:51 +0530

    Main component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import Main from './Main';
 import Name from './Name';
 import SpecialCounter from './SpecialCounter';
 
 function App() {
     return <div>
         <h1>Hello React</h1>
-        <Name name="John" dept="IT" />
-        <Name name="Joseph" dept="HR" />
-        <SpecialCounter count={100} />
-        <SpecialCounter count={10} />
-        <SpecialCounter count={0} />
+        <Main />
     </div>
 }
 
---
+++ src/Main.js
@@ -0,0 +1,7 @@
+import SpecialCounter from "./SpecialCounter";
+
+function Main() {
+    return <SpecialCounter count={10} />
+}
+
+export default Main;
