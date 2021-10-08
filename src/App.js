3c66315a36d6a14064c057e7beef5746bec45156
Mujib <mujib517@gmail.com>
2021-10-08 10:24:14 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Counter from './Counter';
+import Name from './Name';
 
 function App() {
     return <div>
         <h1>Hello React</h1>
+        <Name name="John" dept="IT" />
+        <Name name="Joseph" dept="HR" />
         <Counter />
     </div>
 }
---
+++ src/Name.js
@@ -0,0 +1,9 @@
+// properties props
+function Name(props) {
+    return <div>
+        <h1>{props.name}</h1>
+        <h4>{props.dept}</h4>
+    </div>;
+}
+
+export default Name;
