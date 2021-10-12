a8a9c07ca459f07456ae6202850a418b41cb02e0
Mujib <mujib517@gmail.com>
2021-10-12 11:12:47 +0530

    Bootstrap card component

--- src/User.js
+++ src/User.js
@@ -1,11 +1,20 @@
 function User(props) {
     const { user } = props;
-    
-    return <>
-        <h3>{user.login}</h3>
-        <h4>{user.type}</h4>
-        <hr />
-    </>
+
+    return <div className="row mt-3">
+        <div className="col-3">
+            <div class="card">
+                <img width="200" height="200" src={user.avatar_url} class="card-img-top" />
+                <div class="card-body">
+                    <h5 class="card-title">{user.login}</h5>
+                    <p class="card-text">{user.type}</p>
+                    <a href="#" class="btn btn-sm btn-primary">
+                        Select
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default User;
