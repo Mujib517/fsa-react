8131c8ce883e76ebc8cc016aee06d242f5fd3881
Mujib <mujib517@gmail.com>
2021-10-20 10:42:36 +0530

    Pagination of books

--- src/books/Books.js
+++ src/books/Books.js
@@ -29,21 +29,54 @@ import Book from './Book';
 // hooks
 const Books = () => {
 
-    const [books, setBooks] = useState([]);
+    const [data, setData] = useState({ books: [], metadata: {} });
+    const [pageIndex, setPageIndex] = useState(0);
+    const limit = 2;
 
     useEffect(async () => {
         try {
-            const res = await axios.get('https://my-node-api-demo-2.herokuapp.com/api/books');
-            setBooks(res.data.books);
+            const url = `https://my-node-api-demo-2.herokuapp.com/api/books?pageIndex=${pageIndex}&limit=${limit}`
+            const res = await axios.get(url);
+            setData(res.data);
         } catch (e) {
             // TODO: error handling
             console.log(e);
         }
-    }, []);
+    }, [pageIndex]);
+
+    const onPrev = () => {
+        if (pageIndex !== 0) {
+            let currPage = pageIndex;
+            setPageIndex(--currPage);
+        }
+    }
+
+    const onNext = async () => {
+        let currPage = pageIndex;
+        setPageIndex(++currPage);
+    }
+
+    const isLastPage = () => pageIndex === data.metadata.totalPages - 1;
+    const isFirstPage = () => pageIndex === 0;
 
     return <div>
         <h1>Books</h1>
         <Link to="/books/new" className="btn btn-danger btn-sm">Add New Book</Link>
+        <div className="row mt-3">
+            <div className="col-1">
+                <button disabled={isFirstPage()} onClick={onPrev} class="btn btn-sm btn-primary">
+                    <i className="fa fa-arrow-left"></i>
+                </button>
+            </div>
+            <div className="col-3">
+                Showing Page {pageIndex + 1} of {data.metadata.totalPages}
+            </div>
+            <div className="col-1">
+                <button disabled={isLastPage()} onClick={onNext} class="btn btn-sm btn-primary">
+                    <i className="fa fa-arrow-right"></i>
+                </button>
+            </div>
+        </div>
         <table className="table table-bordered">
             <thead>
                 <th>Id</th>
@@ -51,7 +84,7 @@ const Books = () => {
                 <th>Price</th>
             </thead>
             <tbody>
-                {books.map(book => <Book book={book} />)}
+                {data.books.map(book => <Book book={book} />)}
             </tbody>
         </table>
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import App from './App';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
     <App />, document.getElementById('root'));
