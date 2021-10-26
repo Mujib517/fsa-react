2eb8df3034a3388564c5c93955f2abb8935a1f94
Mujib <mujib517@gmail.com>
2021-10-26 10:12:16 +0530

    NavLink compnent instead of Link

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import userService from './services/userService';
 
 function Header(props) {
@@ -9,31 +9,31 @@ function Header(props) {
     }
 
     return <>
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-            <div class="container-fluid">
-                <div class="navbar-collapse" id="navbarTogglerDemo01">
-                    <Link to="/" class="navbar-brand">FSA</Link>
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/">Home</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <div className="navbar-collapse" id="navbarTogglerDemo01">
+                    <NavLink to="/" className="navbar-brand">FSA</NavLink>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <NavLink exact activeClassName="active" className="nav-link" to="/">Home</NavLink>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/about">About</Link>
+                        <li className="nav-item">
+                            <NavLink activeClassName="active" className="nav-link" to="/about">About</NavLink>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/users">Users</Link>
+                        <li className="nav-item">
+                            <NavLink activeClassName="active" className="nav-link" to="/users">Users</NavLink>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/books">Books</Link>
+                        <li className="nav-item">
+                            <NavLink activeClassName="active" className="nav-link" to="/books">Books</NavLink>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/contact">Contact</Link>
+                        <li className="nav-item">
+                            <NavLink activeClassName="active" className="nav-link" to="/contact">Contact</NavLink>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/count">Counter</Link>
+                        <li className="nav-item">
+                            <NavLink activeClassName="active" className="nav-link" to="/count">Counter</NavLink>
                         </li>
                     </ul>
-                    <Link to="/login" className="btn btn-danger btn-sm">Login</Link>
+                    <NavLink to="/login" className="btn btn-danger btn-sm">Login</NavLink>
                     &nbsp;
                     <button onClick={onLogout} className="btn btn-danger btn-sm">Logout</button>
                 </div>
--- src/User.js
+++ src/User.js
@@ -5,12 +5,12 @@ function User(props) {
 
     return <div className="row mt-3">
         <div className="col-3">
-            <div class="card">
-                <img alt="avatar" width="200" height="200" src={user.avatar_url} class="card-img-top" />
-                <div class="card-body">
-                    <h5 class="card-title">{user.login}</h5>
-                    <p class="card-text">{user.type}</p>
-                    <Link to={`/users/${user.login}`} class="btn btn-sm btn-primary">
+            <div className="card">
+                <img alt="avatar" width="200" height="200" src={user.avatar_url} className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title">{user.login}</h5>
+                    <p className="card-text">{user.type}</p>
+                    <Link to={`/users/${user.login}`} className="btn btn-sm btn-primary">
                         Select
                     </Link>
                 </div>
