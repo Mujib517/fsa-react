2b48353633bc9274300693e330a0893eb22d3e04
Mujib <mujib517@gmail.com>
2021-10-26 10:57:12 +0530

    StrictMode

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import App from './App';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import React from 'react';
 
 ReactDOM.render(
-    <App />, document.getElementById('root'));
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    , document.getElementById('root'));
